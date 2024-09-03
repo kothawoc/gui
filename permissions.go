080f38cd5754ededec5d9c4230f5dc433b4467ce
Kothawoc <178730778+kothawoc@users.noreply.github.com>
2024-09-03 11:08:23 +0200

    improved add group pane, added permissions

---
+++ permissions.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/lang"
+	"fyne.io/fyne/v2/widget"
+)
+
+type PermsT struct {
+	Idx                                  int
+	Vbox, hbox                           *fyne.Container
+	Text                                 *widget.Entry
+	Read, Reply, Post, Cancel, Supersede fyne.Widget
+	Remove                               *widget.Button
+}
+type PermsMgr struct {
+	Form  *widget.Form
+	Vbox  *fyne.Container
+	Items []PermsT
+}
+
+func NewPemsMgr(f *widget.Form) PermsMgr {
+	p := PermsMgr{
+		Form:  f,
+		Vbox:  container.NewVBox(),
+		Items: make([]PermsT, 0),
+	}
+	return p
+}
+
+func (p *PermsMgr) Render() {
+	p.Vbox.RemoveAll()
+	for idx, item := range p.Items {
+		item.Idx = idx
+		item.Remove.OnTapped = func() {
+			p.Items = append(p.Items[:idx], p.Items[idx+1:]...)
+			p.Render()
+		}
+		p.Vbox.Add(item.Vbox)
+	}
+
+	p.Vbox.Add(widget.NewButton("+", func() {
+		p.Add()
+	}))
+
+	p.Vbox.Refresh()
+	p.Form.Refresh()
+	if content != nil {
+		content.Refresh()
+	}
+}
+
+func NewCheck(s string, checked bool) fyne.Widget {
+	cb := widget.NewCheck(s, func(i bool) {})
+	if checked {
+		cb.Checked = true
+	}
+	return cb
+}
+
+func (p *PermsMgr) Add() {
+
+	//{"post", "read", "reply", "cancel", "supersede"}
+	labelSupersede := lang.L("Supersede")
+	labelCancel := lang.L("Cancel")
+	labelRead := lang.L("Read")
+	labelReply := lang.L("Reply")
+	labelPost := lang.L("Post")
+
+	newItem := PermsT{
+		Vbox:      container.NewVBox(),
+		hbox:      container.NewHBox(),
+		Text:      widget.NewEntry(),
+		Read:      NewCheck(labelRead, true),
+		Reply:     NewCheck(labelReply, true),
+		Post:      NewCheck(labelPost, false),
+		Cancel:    NewCheck(labelCancel, false),
+		Supersede: NewCheck(labelSupersede, false),
+		Remove:    widget.NewButton("-", func() {}),
+	}
+	newItem.hbox.Add(newItem.Read)
+	newItem.hbox.Add(newItem.Reply)
+	newItem.hbox.Add(newItem.Post)
+	newItem.hbox.Add(newItem.Cancel)
+	newItem.hbox.Add(newItem.Supersede)
+	newItem.hbox.Add(widget.NewLabel("    "))
+	newItem.hbox.Add(newItem.Remove)
+
+	newItem.Vbox.Add(newItem.Text)
+	newItem.Vbox.Add(newItem.hbox)
+
+	p.Items = append(p.Items, newItem)
+	p.Render()
+}
